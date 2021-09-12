08432f9ee30288fd07eb2e522ac307b7c285939c
Daniel <itzdanielmichael@gmail.com>
2021-09-12 13:02:21 -0400

    update MarketList

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React from 'react';
 
-const Error = () => <div>Error</div>;
+const Error = ({ errors }) => (
+  <pre classname="error">
+    {errors.map((err, idx) => (
+      <div key={idx}>{err.message}</div>
+    ))}
+  </pre>
+);
 
 export default Error;

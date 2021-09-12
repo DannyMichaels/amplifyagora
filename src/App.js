709624fc3fd9165bdce0941319dd63f13c4b2834
Daniel <itzdanielmichael@gmail.com>
2021-09-12 11:06:05 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
-import "./App.css";
+import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react';
+import React from 'react';
+import './App.css';
 
-class App extends React.Component {
-  state = {};
-
-  render() {
-    return <div>App</div>;
-  }
+function App() {
+  return <div>App</div>;
 }
 
-export default App;
+const theme = {
+  ...AmplifyTheme,
+  navBar: {
+    ...AmplifyTheme.navBar,,
+    backgroundColor: "#ffc0cb",
+  },
+  button: {
+    ...AmplifyTheme.button,
+    // backgroundColor: '#f90',
+    backgroundColor: 'var(--amazonOrange)',
+  },
+  sectionBody: {
+    ...AmplifyTheme.sectionBody,
+    padding: '5px',
+  },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: 'var(--squidInk)',
+  },
+};
+
+export default withAuthenticator(App, true, [], null, theme);

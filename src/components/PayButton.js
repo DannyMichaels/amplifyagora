a4fc2823e70529a30b520eb9ed434f4039c9a50f
Daniel <itzdanielmichael@gmail.com>
2021-09-14 14:42:52 -0400

    upd stripe

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-
+import { API } from 'aws-amplify';
 // import { Notification, Message } from "element-react";
 
 const stripeConfig = {
@@ -9,15 +9,29 @@ const stripeConfig = {
 };
 
 const PayButton = ({ product, currentUser }) => {
+  const handleCharge = async (token) => {
+    try {
+      const result = await API.post('orderlambda', '/charge', {
+        body: {
+          token,
+        },
+      });
+      console.log({ result });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <StripeCheckout
+      token={handleCharge}
       email={currentUser.attributes.email}
       name={product.description}
       amount={product.price}
       billingAddress={product.shipped}
       shippingAddress={product.shipped}
       currency={stripeConfig.currency}
-      stripkeyKey={stripeConfig.publishableAPIKey}
+      stripeKey={stripeConfig.publishableAPIKey}
       locale="auto"
       allowRememberMe={false}
     />

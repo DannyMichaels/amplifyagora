2463cf6246f4c1aa2733cc41331493ae1bfdd979
Daniel <itzdanielmichael@gmail.com>
2021-09-15 10:35:11 -0400

    upd

--- src/pages/MarketPage.js
+++ src/pages/MarketPage.js
@@ -10,146 +10,19 @@ import NewProduct from './../components/NewProduct';
 import Product from './../components/Product';
 import { formatProductDate } from './../utils/index';
 
+import useFetchMarketData from './../hooks/useFetchMarketData';
 // need to add subscriptions so we don't have to refresh to see updated state
 // they will execute after the mutations
-import {
-  onCreateProduct,
-  onUpdateProduct,
-  onDeleteProduct,
-} from './../graphql/subscriptions';
-
-const getMarket = /* GraphQL */ `
-  query GetMarket($id: ID!) {
-    getMarket(id: $id) {
-      id
-      name
-      products {
-        items {
-          id
-          description
-          price
-          shipped
-          owner
-          file {
-            key
-          }
-          createdAt
-          updatedAt
-        }
-        nextToken
-      }
-      tags
-      owner
-      createdAt
-      updatedAt
-    }
-  }
-`;
 
 export default function MarketPage() {
-  const [market, setMarket] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isMarketOwner, setIsMarketOwner] = useState(false);
   const { currentUser } = useStateValue();
   const { marketId } = useParams();
 
-  const getUser = async () => {
-    return await Auth.currentUserInfo();
-  };
-
-  const checkMarketOwner = async (market) => {
-    if (currentUser) {
-      setIsMarketOwner(currentUser.username === market?.owner);
-    }
-  };
-
-  const handleGetMarket = async () => {
-    const input = {
-      id: marketId,
-    };
-
-    const result = await API.graphql(graphqlOperation(getMarket, input));
-    let market = result.data.getMarket;
-    setMarket(market);
-    checkMarketOwner(market);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    handleGetMarket();
-
-    let createProductListener, deleteProductListener, updateProductListener;
-
-    const activateProductListeners = async () => {
-      createProductListener = API.graphql(
-        graphqlOperation(onCreateProduct, { owner: (await getUser()).username })
-      ).subscribe({
-        next: (productData) => {
-          console.log({ productData });
-          const createdProduct = productData.value.data.onCreateProduct;
-
-          setMarket((prevState) => {
-            const prevProducts = prevState.products.items.filter(
-              (item) => item.id !== createdProduct.id
-            );
-            const updatedProducts = [createdProduct, ...prevProducts];
-            let updatedMarket = { ...prevState };
-            updatedMarket.products.items = updatedProducts;
-            return updatedMarket;
-          });
-        },
-      });
-
-      updateProductListener = API.graphql(
-        graphqlOperation(onUpdateProduct, {
-          owner: (await getUser()).username,
-        })
-      ).subscribe({
-        next: (productData) => {
-          const updatedProduct = productData.value.data.onUpdateProduct;
-
-          let marketCopy = [...market];
-          const updatedProducts = marketCopy.products.items.map((item) =>
-            item.id === updatedProduct.id ? updatedProduct : item
-          );
-
-          marketCopy.products.items = updatedProducts;
-
-          setMarket(marketCopy);
-        },
-      });
-
-      deleteProductListener = API.graphql(
-        graphqlOperation(onDeleteProduct, { owner: (await getUser()).username })
-      ).subscribe({
-        next: (productData) => {
-          const deletedProduct = productData.value.data.onDeleteProduct;
-
-          setMarket((prevState) => {
-            const updatedProducts = prevState.products.items.filter(
-              (item) => item.id !== deletedProduct.id
-            );
-
-            let updatedMarket = { ...prevState };
-            updatedMarket.products.items = updatedProducts;
-            return updatedMarket;
-          });
-        },
-      });
-    };
-
-    activateProductListeners();
-
-    return () => {
-      // remove the listener on unmount
-
-      createProductListener?.unsubscribe();
-      updateProductListener?.unsubscribe();
-      deleteProductListener?.unsubscribe();
-    };
-
-    // eslint-disable-next-line
-  }, []);
+  const { market, isMarketOwner, isLoading } = useFetchMarketData({
+    userAttributes: currentUser.attributes,
+    user: currentUser,
+    marketId,
+  });
 
   if (isLoading) {
     return <Loading fullScreen={true} />;
